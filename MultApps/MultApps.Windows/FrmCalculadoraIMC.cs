0a1a31ebb30ee41aafcda8e407d4cec02c2a5b06
Welton Castoldi <weltoncastoldi@hotmail.com>
2025-02-27 21:19:29 -0300

    feat: Adiciona formulário da calculadora de IMC #3

---
+++ MultApps/MultApps.Windows/FrmCalculadoraIMC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultApps.Windows
+{
+    public partial class FrmCalculadoraIMC : Form
+    {
+        public FrmCalculadoraIMC()
+        {
+            InitializeComponent();
+        }
+    }
+}
